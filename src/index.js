b2aaf40caa53ade264db777f37eab27ac16243f8
Jhon fredy rincon <fredy88x@yahoo.es>
2022-11-15 18:50:15 -0500

    seasons modify.

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SeasonDisplay from "./SeasonDisplay";
+
+class App extends React.Component{
+
+    state = {lat: null, errorMessage: ''};
+
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(position=> {this.setState({lat:position.coords.latitude})}, err => {this.setState({errorMessage: err.message})});
+    }
+
+    componentDidUpdate(){
+        console.log("My component was just update");
+    }
+
+    render(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+        return <div>Loading!</div>
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
